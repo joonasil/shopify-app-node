4b9d2dc60c96a050f5cfe587430f99c4bc448947
Joonas <joonas.ilvonen@eeco.fi>
2022-03-03 23:02:49 +0200

    format files

--- server/handlers/mutations/get-one-time-url.js
+++ server/handlers/mutations/get-one-time-url.js
@@ -4,21 +4,21 @@ import { gql } from "apollo-boost";
 export function ONETIME_CREATE(url) {
   return gql`
     mutation {
-      appPurchaseOneTimeCreate(
-        name: "test"
-        price: { amount: 10, currencyCode: USD }
-        returnUrl: "${url}"
-        test: true
-      ) {
-        userErrors {
-          field
-          message
-        }
-        confirmationUrl
-        appPurchaseOneTime {
-          id
-        }
-      }
+		appPurchaseOneTimeCreate(
+			name: "test"
+			price: { amount: 10, currencyCode: USD }
+			returnUrl: "${url}"
+			test: true
+		) {
+			userErrors {
+			field
+			message
+			}
+			confirmationUrl
+			appPurchaseOneTime {
+			id
+			}
+		}
     }
   `;
 }
